939d584d338a8c485802aad21f6d979d235a2cc7
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2018-10-28 19:36:23 +0900

    putsと早期リターン

--- common.c
+++ common.c
@@ -2,17 +2,23 @@
 #include<stdlib.h>
 #include<string.h>
 
+enum SEX{
+    SEX_MAN,
+    SEX_WOMAN
+};
+
 struct koza {
-    char sei[20];
-    char mei[20];
-    int sex; //sex=0:man, sex=1:woman
-    unsigned int birthday;
-    char pass[20];
-    char pass_check[20];
     char id[20];
+    char pass[20];
+    char sei[20];
+    char mei[20]; 
+    enum SEX sex; 
+    unsigned int birthday;
     int freeze;
     unsigned int money;
-}account;
+
+    char pass_check[20];
+};
 
 //FIXME:固定長
 struct koza all_koza_data[256];
@@ -20,16 +26,17 @@ unsigned int koza_number;
 
 void input(),output(),send(),iolog(),account_info();
 int search_koza_index();
+
 int main(){
     FILE *fp;
     fp = fopen("info.txt","r");
     koza_number=0;
-    while(fscanf(fp, "%s %s %s %s %d %d %d %d", all_koza_data[koza_number].id,all_koza_data[koza_number].pass,all_koza_data[koza_number].sei,all_koza_data[koza_number].mei,&all_koza_data[koza_number].sex,&all_koza_data[koza_number].birthday,&all_koza_data[koza_number].freeze,&all_koza_data[koza_number].money)!=EOF){
+    while(fscanf(fp, "%s %s %s %s %u %d %d %d", all_koza_data[koza_number].id,all_koza_data[koza_number].pass,all_koza_data[koza_number].sei,all_koza_data[koza_number].mei,&all_koza_data[koza_number].sex,&all_koza_data[koza_number].birthday,&all_koza_data[koza_number].freeze,&all_koza_data[koza_number].money)!=EOF){
         koza_number++; 
     }										//info.txt内全部読み取り&口座のカウント
     fclose(fp);
     for(;;){
-        printf("1.入金 2.出金 3.送金 4.ログの表示 5.アカウント情報 6.終了\n");
+        puts("1.入金 2.出金 3.送金 4.ログの表示 5.アカウント情報 6.終了");
 
         //FIXME:ログイン後の口座番号
         char koza_id[] = "0000000";
@@ -37,41 +44,41 @@ int main(){
 
         int command;
         if(scanf("%d",&command)!=1){
-            printf("Error:1〜6の数字を入力してください\n");
+            puts("Error:1〜6の数字を入力してください");
             scanf("%*s");
-        }else{
-            switch(command){
-                case 1:
-                    input(koza_index);
-                    break;
-                case 2:
-                    output(koza_index);
-                    break;
-                case 3:
-                    send(koza_index);
-                    break; 
-                case 4:
-                    iolog(koza_id);
-                    break;
-                case 5:
-                    fp=fopen("info.txt","w");
-                    for(int i=0;i<koza_number;i++){
-                        fprintf(fp, "%s %s %s %s %d %d %d %d \n", all_koza_data[i].id, all_koza_data[i].pass, all_koza_data[i].sei, all_koza_data[i].mei, all_koza_data[i].sex, all_koza_data[i].birthday, all_koza_data[i].freeze, all_koza_data[i].money);
-                    }						//info.txtに全部書き出し
-                    account_info(koza_index);
-                    fclose(fp);
-                    break;
-                case 6:
-                    fp=fopen("info.txt","w");
-                    for(int i=0;i<koza_number;i++){
-                        fprintf(fp, "%s %s %s %s %d %d %d %d \n", all_koza_data[i].id, all_koza_data[i].pass, all_koza_data[i].sei, all_koza_data[i].mei, all_koza_data[i].sex, all_koza_data[i].birthday, all_koza_data[i].freeze, all_koza_data[i].money);
-                    }
-                    fclose(fp);
-                    exit(1);
-                    break;
-                default:
-                    printf("Error:1〜6の数字を入力してください\n");
-            }
+            continue;
+        }
+        switch(command){
+            case 1:
+                input(koza_index);
+                break;
+            case 2:
+                output(koza_index);
+                break;
+            case 3:
+                send(koza_index);
+                break; 
+            case 4:
+                iolog(koza_id);
+                break;
+            case 5:
+                fp=fopen("info.txt","w");
+                for(int i=0;i<koza_number;i++){
+                    fprintf(fp, "%s %s %s %s %d %d %d %d \n", all_koza_data[i].id, all_koza_data[i].pass, all_koza_data[i].sei, all_koza_data[i].mei, all_koza_data[i].sex, all_koza_data[i].birthday, all_koza_data[i].freeze, all_koza_data[i].money);
+                }						//info.txtに全部書き出し
+                account_info(koza_index);
+                fclose(fp);
+                break;
+            case 6:
+                fp=fopen("info.txt","w");
+                for(int i=0;i<koza_number;i++){
+                    fprintf(fp, "%s %s %s %s %d %d %d %d \n", all_koza_data[i].id, all_koza_data[i].pass, all_koza_data[i].sei, all_koza_data[i].mei, all_koza_data[i].sex, all_koza_data[i].birthday, all_koza_data[i].freeze, all_koza_data[i].money);
+                }
+                fclose(fp);
+                exit(1);
+                break;
+            default:
+                puts("Error:1〜6の数字を入力してください");
         }
     }
 }
@@ -88,73 +95,81 @@ int search_koza_index(char* id){
 
 
 void input(int koza_index){
-    printf("入金する額を入力してください\n");
+    puts("入金する額を入力してください");
     int amount; 
     if(scanf("%d",&amount)!=1){
-        printf("Error:数字を入力してください\n");
+        puts("Error:数字を入力してください");
         scanf("%*s");
-    }else{	
-        all_koza_data[koza_index].money += amount;
-        FILE *fp = fopen("iolog.txt","a");
-        fprintf(fp, "+%d	%d	%s\n",amount,all_koza_data[koza_index].money,all_koza_data[koza_index].id);
-        fclose(fp);
+        return;
     }
+    all_koza_data[koza_index].money += amount;
+
+    FILE *fp = fopen("iolog.txt","a");
+    fprintf(fp, "+%d	%d	%s\n",amount,all_koza_data[koza_index].money,all_koza_data[koza_index].id);
+    fclose(fp);
 }
 
 void output(int koza_index){
-    printf("出金する額を入力してください\n");
+    puts("出金する額を入力してください");
     int amount;
     if(scanf("%d",&amount)!=1){
-        printf("Error:数字を入力してください\n");
+        puts("Error:数字を入力してください");
         scanf("%*s");
-    }else{
-        FILE *fp = fopen("iolog.txt","a");
-        if(all_koza_data[koza_index].money < amount) printf("Error:残高不足です\n");
-        else{
-            all_koza_data[koza_index].money -= amount;
-            fprintf(fp, "-%d	%d	%s\n",amount,all_koza_data[koza_index].money,all_koza_data[koza_index].id);
-            fclose(fp);
-        }
+        return;
     }
+    if(all_koza_data[koza_index].money < amount) {
+        puts("Error:残高不足です");
+        return;
+    }
+    all_koza_data[koza_index].money -= amount;
+
+    FILE *fp = fopen("iolog.txt","a");
+    fprintf(fp, "-%d	%d	%s\n",amount,all_koza_data[koza_index].money,all_koza_data[koza_index].id);
+    fclose(fp);
 }
 
 
 void send(int koza_index){
-    printf("送金先の口座番号を入力してください\n");
-    int amount,dst_id;
-    char k[8];
-    scanf("%s",k);
-    dst_id=search_koza_index(k);
+    puts("送金先の口座番号を入力してください");
+    char koza_id[8];
+    scanf("%s",koza_id);
+    int dst_id=search_koza_index(koza_id);
     if(dst_id==-1){
-        printf("Error:存在しない口座です\n");
-    }else if(all_koza_data[dst_id].freeze==1){
-        printf("Error:凍結されている口座です\n");
-    }else{
-        printf("送金する額を入力してください\n");
-        if(scanf("%d",&amount)!=1){
-            printf("Error:数字を入力してください\n");
-            scanf("%*s");
-        }else{	
-            if(all_koza_data[koza_index].money < amount) printf("Error:残高不足です\n");
-            else{
-                all_koza_data[koza_index].money -= amount;
-                all_koza_data[dst_id].money += amount;
-                FILE *fp = fopen("iolog.txt","a");
-                fprintf(fp, "-%d	%d	%s \n+%d	%d	%s \n",amount,all_koza_data[koza_index].money,all_koza_data[koza_index].id,amount,all_koza_data[dst_id].money,all_koza_data[dst_id].id);
-                fclose(fp);
-            }
-        }
+        puts("Error:存在しない口座です");
+        return;
     }
+    if(all_koza_data[dst_id].freeze){
+        puts("Error:凍結されている口座です");
+        return;
+    }
+
+    puts("送金する額を入力してください");
+    int amount;
+    if(scanf("%d",&amount)!=1){
+        puts("Error:数字を入力してください");
+        scanf("%*s");
+        return;
+    }
+    if(all_koza_data[koza_index].money < amount){
+        puts("Error:残高不足です");
+        return;
+    }
+    all_koza_data[koza_index].money -= amount;
+    all_koza_data[dst_id].money += amount;
+
+    FILE *fp = fopen("iolog.txt","a");
+    fprintf(fp, "-%d	%d	%s \n+%d	%d	%s \n",amount,all_koza_data[koza_index].money,all_koza_data[koza_index].id,amount,all_koza_data[dst_id].money,all_koza_data[dst_id].id);
+    fclose(fp);
 }
 
 
 
-void iolog(char* koza_id){
+void iolog(char* target_koza_id){
     FILE *fp = fopen("iolog.txt","r");
-    char a[10],c[8];
-    int b;
-    while(fscanf(fp,"%s	%d	%s",a,&b,c)!=EOF){					//とりあえず全部読み取り
-        if(strcmp(c,koza_id)==0) printf("%s	%d \n",a,b); 		//口座番号が一致すればprintf 
+    char amount[10],koza_id[8];
+    int balance;
+    while(fscanf(fp,"%s	%d	%s",amount,&balance,koza_id)!=EOF){					//とりあえず全部読み取り
+        if(strcmp(koza_id,target_koza_id)==0) printf("%s	%d \n",amount,balance); 		//口座番号が一致すればprintf 
     }
     fclose(fp);
 }
@@ -165,8 +180,11 @@ void account_info(int koza_index){
     printf("	姓 : %s \n",all_koza_data[koza_index].sei);
     printf("	名 : %s \n",all_koza_data[koza_index].mei);
     printf("	性別 : ");
-    if (all_koza_data[koza_index].sex == 0) printf("男性\n");
-    else printf("女性\n");
+    if (all_koza_data[koza_index].sex ==SEX_MAN){
+        puts("男性");
+    } else {
+        puts("女性");
+    }
     printf("	生年月日 : %d \n",all_koza_data[koza_index].birthday);
     printf("	残高 : %d円 \n",all_koza_data[koza_index].money);
 }
