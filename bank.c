48b36136d0c78a19afbb97b20f8cf6d1fddab952
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2018-10-29 01:25:23 +0900

    長すぎるのを改行

--- bank.c
+++ bank.c
@@ -11,7 +11,16 @@ int current_koza_index;
 int main(){
     FILE *fp= fopen("info.txt","r");
     number_of_koza=0;
-    while(fscanf(fp, "%s %s %s %s %u %d %d %d", all_koza_data[number_of_koza].id,all_koza_data[number_of_koza].pass,all_koza_data[number_of_koza].sei,all_koza_data[number_of_koza].mei,&all_koza_data[number_of_koza].sex,&all_koza_data[number_of_koza].birthday,&all_koza_data[number_of_koza].freeze,&all_koza_data[number_of_koza].money)!=EOF){
+    while(fscanf(fp, "%s %s %s %s %u %d %d %d", 
+                all_koza_data[number_of_koza].id,
+                all_koza_data[number_of_koza].pass,
+                all_koza_data[number_of_koza].sei,
+                all_koza_data[number_of_koza].mei,
+                &all_koza_data[number_of_koza].sex,
+                &all_koza_data[number_of_koza].birthday,
+                &all_koza_data[number_of_koza].freeze,
+                &all_koza_data[number_of_koza].money
+                )!=EOF){
         number_of_koza++; 
     }
     fclose(fp);
@@ -32,7 +41,15 @@ int main(){
 
     fp= fopen("info.txt","w");
     for(int i=0;i<number_of_koza;i++){
-        fprintf(fp, "%s %s %s %s %d %d %d %d \n", all_koza_data[i].id, all_koza_data[i].pass, all_koza_data[i].sei, all_koza_data[i].mei, all_koza_data[i].sex, all_koza_data[i].birthday, all_koza_data[i].freeze, all_koza_data[i].money);
+        fprintf(fp, "%s %s %s %s %d %d %d %d \n", 
+                all_koza_data[i].id,
+                all_koza_data[i].pass,
+                all_koza_data[i].sei,
+                all_koza_data[i].mei,
+                all_koza_data[i].sex,
+                all_koza_data[i].birthday,
+                all_koza_data[i].freeze,
+                all_koza_data[i].money);
     }
     fclose(fp);
 }
